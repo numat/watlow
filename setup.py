ff5346c6ea16cbed1b3797f9c0956d5a2bae03d7
Patrick Fuller <patrickfuller@gmail.com>
2019-06-25 17:16:58 -0500

    Version bump for bugfix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as in_file:
 
 setup(
     name="watlow",
-    version="0.1.0",
+    version="0.1.1",
     description="Python driver for Watlow EZ-Zone temperature controllers.",
     long_description=long_description,
     long_description_content_type='text/markdown',
