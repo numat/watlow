f7a4f76abdf41883c33b2ae43be147f6340d8d07
JamesJeffryes <jamesgjeffryes@gmail.com>
2021-03-29 15:42:51 -0500

    allow range of modern pymodbus

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 """Install parameters for CLI and python import."""
+from sys import version_info
 from setuptools import setup
+
+if version_info < (3, 6):
+    raise ImportError("This module requires Python >=3.6 for asyncio support")
+if version_info >= (3, 10):
+    raise ImportError("This module depends on pymodbus, which is incompatible with Python 3.10")
 
 with open('README.md', 'r') as in_file:
     long_description = in_file.read()
 
 setup(
     name="watlow",
-    version="0.2.4b1",
+    version="0.2.4",
     description="Python driver for Watlow EZ-Zone temperature controllers.",
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -14,7 +20,7 @@ setup(
     author="Patrick Fuller",
     author_email="pat@numat-tech.com",
     packages=["watlow"],
-    install_requires=["pymodbus==2.5.0",
+    install_requires=["pymodbus>=2.4.0",
                       "pyserial",
                       "crcmod"],
     entry_points={
@@ -27,6 +33,10 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         "Topic :: Scientific/Engineering :: Human Machine Interfaces",
         "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)"
     ]
